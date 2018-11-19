bac4ccd5283818d8c2bd8ccda1adfd3548d27815
Nicolas Alvarez Llobera <nalvarezllobera@navent.com>
2018-11-19 18:09:45 -0300

    agrego @Data

--- src/main/java/com/example/polling/model/DateAudit.java
+++ src/main/java/com/example/polling/model/DateAudit.java
@@ -1,6 +1,7 @@
 package com.example.polling.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,6 +14,7 @@ import java.time.Instant;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt","updatedAt"}, allowGetters = true)
+@Data
 public abstract class DateAudit {
 
     @CreatedDate
